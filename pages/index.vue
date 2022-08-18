ada822f5dde62d83df4bddc6e70dd5a27c2f7bd3
amita-kale <amitakale297@gmail.com>
2022-08-18 17:38:06 +0530

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>Hello world</h3>
+        <!-- <studentForm></studentForm> -->
+    </div>
+</template>
+<script>
+
+// import studentForm from '/components/studentform.vue';
+
+export default {
+    name: 'index',
+    // components: { studentForm }
+}
+</script>
