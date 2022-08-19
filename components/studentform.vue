00e8cbe06c7bb2847f3a259ad4dce952d2056428
amita-kale <amitakale297@gmail.com>
2022-08-19 11:24:31 +0530

    feat: displayed table and search box

--- components/studentform.vue
+++ components/studentform.vue
@@ -39,7 +39,23 @@
                 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Submit</button>
                 </form>
             </div>
-            <div class="bg-gray-300 col-span-2">hello
+            <div class="bg-gray-300 col-span-2">
+
+                <input
+                    class="placeholder:italic placeholder:text-slate-400 block bg-white w-50 border border-slate-300 rounded-md m-10 py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                    placeholder="Search for anything..." type="text" name="search" />
+                <table class=" border-black-400 border-separate bg-white  border border-slate-400 m-20">
+                    <tr>
+                        <th>StudentID</th>
+                        <th>FullName</th>
+                        <th>Email</th>
+                        <th>Contact</th>
+                        <th>Address</th>
+                        <th>StudentProfile</th>
+                        <th colspan="2">Action</th>
+                    </tr>
+
+                </table>
 
             </div>
         </div>
